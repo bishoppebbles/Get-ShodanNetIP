4f8ee976cc1510c749fd380fac301c7b1ea42a30
Sam Pursglove <spursglove@gmail.com>
2024-06-07 14:05:15 +0900

    Formatting adjustments

--- Get-ShodanNetIP.ps1
+++ Get-ShodanNetIP.ps1
@@ -49,7 +49,10 @@
 [CmdletBinding()]
 Param (
     [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true, Position=0)]
-    [string]$NetworkInput
+    [string]$NetworkInput,
+
+    [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$false)]
+    [string]$Outfile = 'Shodan_Output.csv'
 )
 
 #  .ExternalHelp Posh-Shodan.Help.xml
@@ -59,16 +62,11 @@ function Set-ShodanAPIKey
     Param
     (
         # Shodan API Key.
-        [Parameter(Mandatory=$true,
-                   ValueFromPipelineByPropertyName=$true,
-                   Position=0)]
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true, Position=0)]
         [string]$APIKey,
 
-        [Parameter(Mandatory=$true,
-                   ValueFromPipelineByPropertyName=$true,
-                   Position=1)]
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true, Position=1)]
         [securestring]$MasterPassword
-
     )
 
     Begin
@@ -97,8 +95,7 @@ function Set-ShodanAPIKey
         $ConfigName = 'api.key'
         $saltname   = 'salt.rnd'
         
-        if (!(Test-Path -Path "$($env:AppData)\$FolderName"))
-        {
+        if (!(Test-Path -Path "$($env:AppData)\$FolderName")) {
             Write-Verbose -Message 'Seems this is the first time the config has been set.'
             Write-Verbose -Message "Creating folder $("$($env:AppData)\$FolderName")"
             New-Item -ItemType directory -Path "$($env:AppData)\$FolderName" | Out-Null
@@ -108,8 +105,9 @@ function Set-ShodanAPIKey
         "$($EncryptedString)"  | Set-Content  "$($env:AppData)\$FolderName\$ConfigName" -Force
         Set-Content -Value $SaltBytes -Encoding Byte -Path "$($env:AppData)\$FolderName\$saltname" -Force
     }
-    End
-    {}
+    
+    End {
+    }
 }
 
 #  .ExternalHelp Posh-Shodan.Help.xml
@@ -119,134 +117,91 @@ function Search-ShodanHost
     Param
     (
        # Shodan developer API key
-        [Parameter(Mandatory=$false,
-                   ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-                   ParameterSetName = 'Direct')]
-        [string]
-        $APIKey,
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
+        [string]$APIKey,
 
-        [Parameter(Mandatory=$false,
-                   ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-                   ParameterSetName = 'Direct')]
-        [string]
-        $CertificateThumbprint,
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
+        [string]$CertificateThumbprint,
 
-        [Parameter(Mandatory=$true,
-                   ParameterSetName = 'Proxy')]
-        [string]
-        $Proxy,
+        [Parameter(Mandatory=$true, ParameterSetName = 'Proxy')]
+        [string]$Proxy,
  
-        [Parameter(Mandatory=$false,
-                   ParameterSetName = 'Proxy')]
-        [Management.Automation.PSCredential]
-        $ProxyCredential,
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Management.Automation.PSCredential]$ProxyCredential,
 
-        [Parameter(Mandatory=$false,
-                   ParameterSetName = 'Proxy')]
-        [Switch]
-        $ProxyUseDefaultCredentials,
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Switch]$ProxyUseDefaultCredentials,
 
          # Text to query for.
-        [Parameter(Mandatory=$false,
-                   ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-                   ParameterSetName = 'Direct')]
-        [string]
-        $Query = '',
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
+        [string]$Query = '',
 
         #  Find devices located in the given city. It's best combined with the
         # 'Country' filter to make sure you get the city in the country you 
         # want (city names are not always unique).
-        [Parameter(Mandatory=$false,
-                   ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-                   ParameterSetName = 'Direct')]
-        [string]
-        $City,
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
+        [string]$City,
 
         # Narrow results down by country.
-        [Parameter(Mandatory=$false,
-                   ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-                   ParameterSetName = 'Direct')]
-        [string]
-        $Country,
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
+        [string]$Country,
 
         # Latitude and longitude.
-        [Parameter(Mandatory=$false,
-                   ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-                   ParameterSetName = 'Direct')]
-        [string]
-        $Geo,
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
+        [string]$Geo,
 
         # Search for hosts that contain the value in their hostname.
-        [Parameter(Mandatory=$false,
-                   ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-                   ParameterSetName = 'Direct')]
-        [string]
-        $Hostname,
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
+        [string]$Hostname,
         
         # Limit the search results to a specific IP or subnet. It uses CIDR 
         # notation to designate the subnet range.
-        [Parameter(Mandatory=$false,
-                   ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-                   ParameterSetName = 'Direct')]
-        [string]
-        $Net,
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
+        [string]$Net,
 
         # Specific operating systems. Common possible values are: windows,
         # linux and cisco.
-        [Parameter(Mandatory=$false,
-                   ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-                   ParameterSetName = 'Direct')]
-        [string]
-        $OS,
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
+        [string]$OS,
 
         # Search the HTML of the website for the given value.
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Direct')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
         [string]$HTML,
 
         # Find devices based on the upstream owner of the IP netblock.
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Direct')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
         [string]$ISP,
 
         # The network link type. Possible values are: "Ethernet or modem", 
         # "generic tunnel or VPN", "DSL", "IPIP or SIT", "SLIP", "IPSec or
         # "GRE", "VLAN", "jumbo Ethernet", "Google", "GIF", "PPTP", "loopback",
         # "AX.25 radio modem".
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Direct')]
-        [ValidateSet( 'Ethernet or modem', 'generic tunnel or VPN', 'DSL', 
-            'IPIP or SIT', 'SLIP', 'IPSec or GRE', 'VLAN', 'jumbo Ethernet',
-            'Google', 'GIF', 'PPTP', 'loopback', 'AX.25 radio modem')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
+        [ValidateSet( 'Ethernet or modem', 'generic tunnel or VPN', 'DSL', 'IPIP or SIT',
+                      'SLIP', 'IPSec or GRE', 'VLAN', 'jumbo Ethernet', 'Google', 'GIF', 
+                      'PPTP', 'loopback', 'AX.25 radio modem')]
         [string[]]$Link,
 
         #Find NTP servers that had the given IP in their monlist.
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Direct')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
         [string]$NTP_IP,
 
         # Find NTP servers that return the given number of IPs in the initial monlist response.
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Direct')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
         [string]$NTP_IP_Count,
 
         # Find NTP servers that had IPs with the given port in their monlist.
@@ -257,112 +212,84 @@ function Search-ShodanHost
         [int]$NTP_Port,
 
         # Whether or not more IPs were available for the given NTP server.
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Direct')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
         [switch]$NTP_More,
 
         # Find devices based on the owner of the IP netblock.
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Direct')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
         [string]$Org,
 
         # Filter using the name of the software/ product; ex: product:Apache
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Direct')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
         [string]$Product,
 
         # Filter the results to include only products of the given version; ex: product:apache version:1.3.37
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Direct')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
         [string]$Version,
 
         # Search the title of the website.
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Direct')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
         [string]$Title,
 
         # Port number  to narrow the search to specific services.
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Direct')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
         [string]$Port,
 
         # Limit search for data that was collected before the given date in
         # format day/month/year.
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Direct')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
         [string]$Before,
 
         # Limit search for data that was collected after the given date in
         # format day/month/year.
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Direct')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
         [string]$After,
 
         # The page number to page through results 100 at a time. Overrides the
         # "offset" and "limit" parameters if they were provided (default: 1)
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Direct')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
         [int]$Page,
 
         # The positon from which the search results should be returned (default: 0)
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Direct')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
         [int]$Offset,
 
         # The number of results to be returned default(100)
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Direct')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
         [int]$Limit,
 
         # True or False; whether or not to truncate some of the larger fields (default: True)
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Direct')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
         [bool]$Minify = $true,
 
         # A comma-separated list of properties to get summary information on. Property names 
         # can also be in the format of "property:count", where "count" is the number of facets
         # that will be returned for a property (i.e. "country:100" to get the top 100 countries
         # for a search query).
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Proxy')]
-        [Parameter(Mandatory=$false,
-        ParameterSetName = 'Direct')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Proxy')]
+        [Parameter(Mandatory=$false, ParameterSetName = 'Direct')]
         [string]$Facets
 
     )
 
     Begin
     {
-        if (!(Test-Path variable:Global:ShodanAPIKey ) -and !($APIKey))
-        {
+        if (!(Test-Path variable:Global:ShodanAPIKey ) -and !($APIKey)) {
             throw 'No Shodan API Key has been specified or set.'
-        }
-        elseif ((Test-Path -Path variable:Global:ShodanAPIKey ) -and !($APIKey))
-        {
+        
+        } elseif ((Test-Path -Path variable:Global:ShodanAPIKey ) -and !($APIKey)) {
             $APIKey = $Global:ShodanAPIKey
         }
 
@@ -410,32 +337,25 @@ function Search-ShodanHost
         # Set propper request parameters.
         $Body = @{'key'= $APIKey; 'query'= $Query}
 
-        if ($Page)
-        {
+        if ($Page) {
             $Body.Add('page', $Page)
         }
 
-        if ($Offset)
-        {
+        if ($Offset) {
             $Body.Add('offset',$Offset)
         }
 
-        if ($Limit)
-        {
+        if ($Limit) {
             $Body.Add('limit',$Limit)
         }
 
-        if ($Minify)
-        {
+        if ($Minify) {
             $Body.Add('minify','True')
-        }
-        else
-        {
+        } else {
             $Body.Add('minify','False')
         }
 
-        if ($Facets)
-        {
+        if ($Facets) {
             $Body.Add('facets',$Facets)
         }
 
@@ -446,38 +366,31 @@ function Search-ShodanHost
         $Params.add('Uri',[uri]'https://api.shodan.io/shodan/host/search')
 
         # Check if connection will be made thru a proxy.
-        if ($PsCmdlet.ParameterSetName -eq 'Proxy')
-        {
+        if ($PsCmdlet.ParameterSetName -eq 'Proxy') {
             $Params.Add('Proxy', $Proxy)
 
-            if ($ProxyCredential)
-            {
+            if ($ProxyCredential) {
                 $Params.Add('ProxyCredential', $ProxyCredential)
             }
 
-            if ($ProxyUseDefaultCredentials)
-            {
+            if ($ProxyUseDefaultCredentials) {
                 $Params.Add('ProxyUseDefaultCredentials', $ProxyUseDefaultCredentials)
             }
         }
 
         # Check if we will be doing certificate pinning by checking the certificate thumprint.
-        if ($CertificateThumbprint)
-        {
+        if ($CertificateThumbprint) {
             $Params.Add('CertificateThumbprint', $CertificateThumbprint)
         }
     }
-    Process
-    {
-       
+    
+    Process {
         $ReturnedObject = Invoke-RestMethod @Params
-        if ($ReturnedObject)
-        {
-            if ($ReturnedObject.total -ne 0)
-            {
+        if ($ReturnedObject) {
+            if ($ReturnedObject.total -ne 0) {
                 $matches = @()
-                foreach($match in $ReturnedObject.matches)
-                {
+
+                foreach($match in $ReturnedObject.matches) {
                     $match.pstypenames.insert(0,'Shodan.Host.Match')
                     $matches = $matches + $match
                 }
@@ -491,19 +404,18 @@ function Search-ShodanHost
                 $searchobj = [pscustomobject]$properties
                 $searchobj.pstypenames.insert(0,'Shodan.Host.Search')
                 $searchobj
-            }
-            else
-            {
+            
+            } else {
                 Write-Warning -Message 'No matches found.'
             }
         }
     }
-    End 
-    {
+    End {
     }
 }
 
 Add-Type -AssemblyName System.Security
+$output = New-Object System.Collections.ArrayList
 
 [SecureString]$secStrApiKey | Out-Null
 if(-not $secStrApiKey) {
@@ -512,10 +424,9 @@ if(-not $secStrApiKey) {
 }
 
 $data = Get-Content "$NetworkInput" | ForEach-Object {Search-ShodanHost -Net $_}
-$out = New-Object System.Collections.ArrayList
 
 $data.Matches | ForEach-Object {
-    $out.Add([PSCustomObject]@{
+    $output.Add([PSCustomObject]@{
         Product     = $_.product
         Protocol    = $_._shodan.module
         Port        = $_.port
@@ -535,4 +446,4 @@ $data.Matches | ForEach-Object {
     ) | Out-Null
 }
     
-$out | Export-Csv -Path test.csv -NoTypeInformation
+$output | Export-Csv -Path $Outfile -NoTypeInformation
